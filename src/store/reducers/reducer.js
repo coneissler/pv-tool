bb7dff515f833612d5e158154ae493957dada29b
Constantin Eissler <constantineissler@MBP-Windows.fritz.box>
2017-12-09 22:05:14 +0100

    folder structur

---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from '../actions/actions';
+
+const initialState = {
+    x: 1
+};
+
+const reducer = (state = initialState, action) => {
+
+    return null;
+};
+
+export default reducer;
