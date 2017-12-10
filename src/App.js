7d6d6d4448728c0ccc7f5748f8474f1a4d55333f
Constantin Eissler <constantineissler@MBP-Windows.fritz.box>
2017-12-10 15:26:01 +0100

    added basic state+reducers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import classes from './App.css'
+import {connect} from 'react-redux';
+import * as actionTypes from './store/actions/actions'
 
 class App extends Component {
+    state={
+        value: 'a2'
+    };
+
+    onChangeHandler = (event) => {
+        this.setState({
+            value: event.target.value
+        })
+
+
+    };
+
   render() {
       return(
           <div className={classes.Wrapper}>
-              <p className={classes.Header}>PV-Tool<br></br>
+              <p className={classes.Header}><strong>PV-Tool</strong><br></br>
                   <i style={{fontSize: '30px'}}>!!under construction!!</i>
               </p>
+              <form><input value={this.state.value} onChange={(event) => this.onChangeHandler(event)}/></form>
+              <button onClick={() => this.props.enableElement('p' , 1)}>ENABLE ELEMENT</button>
+              <button onClick={() => this.props.disableElement('p', 1)}>DISABLE ELEMENT</button>
+              <button onClick={() => this.props.toggleElement('p', 1)}>TOGGLE ELEMENT</button>
           </div>
 
       );
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        enableElement: (el, size) => dispatch ({type: actionTypes.ENABLE_ELEMENT, element: el, size: size}),
+        disableElement: (el, size) => dispatch ({type: actionTypes.DISABLE_ELEMENT, element: el, size: size}),
+        toggleElement: (el, size) => dispatch ({type: actionTypes.TOGGLE_ELEMENT, element: el, size: size})
+
+    };
+
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,12 +1,71 @@
 import * as actionTypes from '../actions/actions';
 
 const initialState = {
-    x: 1
+    checkSum: 3,
+    a: {
+        1: false,
+        2: true
+    },
+
+    s: {
+        1: false,
+        2: false
+    },
+
+    p: {
+        1: true,
+        2: false
+    }
 };
 
 const reducer = (state = initialState, action) => {
+    let newSum = 0;
+    switch(action.type){
 
-    return null;
+        // enables and updates the checksum
+        case(actionTypes.ENABLE_ELEMENT):
+            newSum = state.checkSum + action.size;
+            if(newSum > 4 || state[action.element][action.size]) return state;
+            return{
+                ...state,
+                checkSum: newSum,
+                [action.element]: {
+                    ...state[action.element],
+                    [action.size]: true
+                }
+            };
+
+        // disables an element and updates the checksum
+        case(actionTypes.DISABLE_ELEMENT):
+            newSum = state.checkSum - action.size;
+            if(!state[action.element][action.size]) return state;
+            return{
+                ...state,
+                checkSum: newSum,
+                [action.element]: {
+                    ...state[action.element],
+                    [action.size]: false
+                }
+            };
+        // assuming if (checksum = 4)=>expand button is disabled
+        // also assuming that 1,2 can never be activ at the same time
+        // Toggle expand, collapse
+        case(actionTypes.TOGGLE_ELEMENT):
+            const updatedElement = {
+                1: !state[action.element][1],
+                2: !state[action.element][2]
+            };
+            newSum = state.checkSum - 1;
+            if(state[action.element][1]) newSum += 2;
+            return{
+                ...state,
+                checkSum: newSum,
+                [action.element]: updatedElement
+
+            };
+
+        default: return state;
+    }
 };
 
 export default reducer;
